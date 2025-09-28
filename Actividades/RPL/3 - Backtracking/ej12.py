dec0ad442d2de84f1402b8bf1bad54d02b12c4bc
AvrilMZ <amorfeo@fi.uba.ar>
2025-09-28 19:28:07 -0300

    Resuelto

--- Actividades/RPL/3 - Backtracking/ej12.py
+++ Actividades/RPL/3 - Backtracking/ej12.py
@@ -1,32 +1,32 @@
 '''
-Modificar el algoritmo anterior para que, dada una lista de enteros positivos L y un entero n, 
-devuelva un subconjunto de L que sume exactamente n, o, en caso de no existir, que devuelva el subconjunto de suma máxima sin superar el valor de n.
+Modificar el algoritmo anterior para que, dada una lista de enteros positivos L y un entero n, devuelva un subconjunto de L que sume exactamente n, o, en caso de no existir, que devuelva el subconjunto de suma máxima sin superar el valor de n.
 '''
 
-def sub_conj(lista, sub, sum_actual, record, num, i_actual):
-	if sum(record) < sum_actual <= num:
-		record.clear()
-		record.extend(sub)
-	if sum_actual == num or i_actual == len(lista):
-		return
-		
-	for i in range(i_actual, len(lista)):
-		if sum_actual + lista[i] <= num:
-			sub.append(lista[i])
-			sub_conj(lista, sub, sum_actual + lista[i], record,  num, i + 1)
-			sub.pop()
-
-	return
+def sub_conj(lista, n, i, actual):
+	sum_actual, sol_actual = actual
+	if i == len(lista) or sum_actual == n:
+		return (sum_actual, sol_actual.copy())
 	
+	num = lista[i]
+	agrego = (0, [])
+	if sum_actual + num <= n:
+		sol_actual.append(num)
+		sum_actual += num
+		agrego = sub_conj(lista, n, i + 1, (sum_actual, sol_actual))
+		sol_actual.pop()
+		sum_actual -= num
+	
+	salteo = sub_conj(lista, n, i + 1, (sum_actual, sol_actual))
+
+	sum_agrego, sol_agrego = agrego
+	sum_salteo, sol_salteo = salteo
+	if sum_agrego > sum_salteo:
+		return (sum_agrego, sol_agrego)
+	return (sum_salteo, sol_salteo)
+
 def max_sumatoria_n(lista, n):
-	if n == 0 or n > sum(lista):
+	if n <= 0:
 		return []
-	elif n == 1:
-		if 1 in lista:
-			return [1]
 
-	record = []
-	sub_conj(lista, [], 0, record, n, 0)
-	return record
-
-# CORREGIR
+	_, sol = sub_conj(lista, n, 0, (0, []))
+	return sol

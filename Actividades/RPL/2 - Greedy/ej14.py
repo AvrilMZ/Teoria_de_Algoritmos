0496898e145d04ff8d2bb55277d8ccfac99f3e55
AvrilMZ <amorfeo@fi.uba.ar>
2025-09-28 20:47:09 -0300

    Resuelto

--- Actividades/RPL/2 - Greedy/ej14.py
+++ Actividades/RPL/2 - Greedy/ej14.py
@@ -25,23 +25,35 @@ Métodos del grafo:
 	str
 '''
 
-def comparten_area(submarino, faro, filas, columnas):
+def comparten_area(submarino, faro):
+	x1, y1 = submarino
 	x2, y2 = faro
+	return (x2 - 2 <= x1 <= x2 + 2) and (y2 - 2 <= y1 <= y2 + 2)
 
-	area = [(x, y) for x in range(max(0, x2 - 2), min(filas, x2 + 3))
-			for y in range(max(0, y2 - 2), min(columnas, y2 + 3))]
-
-	return submarino in area
-
-def esta_iluminado(submarino, faros, filas, columnas):
+def esta_iluminado(submarino, faros):
 	if len(faros) == 0:
 		return False
 	
 	for faro in faros:
-		if comparten_area(submarino, faro, filas, columnas):
+		if comparten_area(submarino, faro):
 			return True
 	
 	return False
+
+def buscar_cobertura(fil, col, submarinos_pendientes):
+	cobertura = set()
+	for submarino in submarinos_pendientes:
+		if comparten_area(submarino, (fil, col)):
+			cobertura.add(submarino)
+	return cobertura
+
+def buscar_submarinos(matriz):
+	submarinos = set()
+	for i in range(len(matriz)):
+		for j in range(len(matriz[0])):
+			if matriz[i][j]:
+				submarinos.add((i, j))
+	return submarinos
 
 # devolver una lista de faros. Cada faro debe ser una tupla con su posición en (x,y)
 # matriz booleana, indica True en las posiciones con submarinos
@@ -49,27 +61,31 @@ def submarinos(matriz):
 	if len(matriz) == 0:
 		return []
 
-	filas = len(matriz)
-	columnas = len(matriz[0])
 	faros = []
-
-	for i in range(0, filas):
-		for j in range(0, columnas):
-			submarino = matriz[i][j]
-			if submarino and not esta_iluminado((i,j), faros, filas, columnas):
-				if i + 2 < filas and j + 2 < columnas:
-					faros.append((i+2,j+2))
-				else:
-					faros.append((i,j))
-
+	while True:
+		max_cobertura = 0
+		mejor_pos = None
+		for i in range(0, len(matriz)):
+			for j in range(0, len(matriz[0])):
+				cobertura = 0
+				for x in range(0, len(matriz)):
+					for y in range(0, len(matriz[0])):
+						if (matriz[x][y] and not esta_iluminado((x, y), faros) and comparten_area((x, y), (i, j))):
+							cobertura += 1
+				if cobertura > max_cobertura:
+					max_cobertura = cobertura
+					mejor_pos = (i, j)
+		if max_cobertura == 0:
+			break
+		faros.append(mejor_pos)
+	
 	return faros
-
-# FALLAN 2 TESTS
 
 '''
 Complejidad:
-	- Recorrer la matriz: O(n * m), siendo n la cantidad de filas y m la cantidad de columnas;
-	- Recorrer por cada submarino todos los faros para verficar si alguno lo ilumina: O(s * k), siendo s la cantidad de submarinos y k la cantidad de faros colocados. Dado que k en el peor de los casos es igual a s podemos escribir la complejidad como O(s^2).
-Por lo que la complejidad final resulta en:
-	O(n * m + s^2)
+	- El algoritmo itera k veces (una por cada faro colocado), siendo k la cantidad de faros necesarios;
+	- En cada iteración, recorre toda la matriz para encontrar la mejor posición: O(n * m);
+	- Por cada posición candidata (i,j), recorre nuevamente la matriz para contar submarinos no iluminados que cubriría: O(n * m);
+	- Por lo que cada iteración es O(n * m * n * m) = O(n^2 * m^2);
+	- Complejidad final: O(k * n^2 * m^2)
 '''

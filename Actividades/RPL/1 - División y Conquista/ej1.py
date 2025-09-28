c46601aaa0430eb497db69c8a8681f4af34b8088
AvrilMZ <amorfeo@fi.uba.ar>
2025-09-28 19:56:09 -0300

    Resuelto

--- Actividades/RPL/1 - División y Conquista/ej1.py
+++ Actividades/RPL/1 - División y Conquista/ej1.py
@@ -6,20 +6,21 @@ Nota sobre RPL: en este ejercicio se pide cumplir la tarea "por división y conq
 no podemos verificarlo de forma automática, pero se busca que se implemente con dicha restricción
 '''
 
-def buscar_elemento_desordenado(arr, izq, der):
-	if izq == der:
-		return arr[izq]
+def buscar_elemento_desordenado(arr, ini, fin):
+	if ini >= fin:
+		return None
+	
+	medio = (ini + fin) // 2
 
-	medio = (izq + der) // 2
-	if medio > 0 and arr[medio] < arr[medio - 1]:
-		return arr[medio]
-	elif medio < len(arr) - 1 and arr[medio] > arr[medio + 1]:
+	if arr[medio] > arr[medio + 1]:
+		if medio - 1 >= 0 and arr[medio + 1] < arr[medio - 1]:
+			return arr[medio + 1]
 		return arr[medio]
 	
-	if arr[medio] >= arr[izq]:
-		return buscar_elemento_desordenado(arr, medio + 1, der)
-	else:
-		return buscar_elemento_desordenado(arr, izq, medio - 1)
+	izq = buscar_elemento_desordenado(arr, ini, medio)
+	if izq:
+		return izq
+	return buscar_elemento_desordenado(arr, medio + 1, fin)
 
 def elemento_desordenado(arr):
 	return buscar_elemento_desordenado(arr, 0, len(arr) - 1)
@@ -28,8 +29,8 @@ def elemento_desordenado(arr):
 Por el teorema maestro:
 	T(n)=AT(n/B)+O(f(n))
 Entonces:
-	T(n)=1T(n/2)+O(1) -> A>=1 y B>1
-	O(1) == n^(log_2(1))
+	T(n)=2T(n/2)+O(1) -> A>=1 y B>1
+	O(1) == n^(log_2(2))
 Por lo tanto la complejidad es:
-	O(log(n))
+	O(n)
 '''

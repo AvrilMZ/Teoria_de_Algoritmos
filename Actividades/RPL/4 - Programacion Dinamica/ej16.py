9fd8275e1a9499671b10ca9bc6ca4051dc981c5a
AvrilMZ <amorfeo@fi.uba.ar>
2025-09-29 00:32:40 -0300

    Resuelto

--- Actividades/RPL/4 - Programacion Dinamica/ej16.py
+++ Actividades/RPL/4 - Programacion Dinamica/ej16.py
@@ -5,9 +5,47 @@ Implementar un algoritmo de programación dinámica que permita resolver el prob
 '''
 
 '''
-En este problema me tengo que forzar a comprar en todos los dias hasta n - 1 y luego verificar en cual tengo la ganancia maxima:
-	OPT[i] = max(0, OPT[i - 1] + P(i) - P(i - 1))
+En este problema me tengo que forzar a comprar en todos los dias hasta n - 1 y forzar a vender en los subsecuentes, para luego verificar en cual tengo la ganancia maxima:
+	OPT[n] = max(0, OPT[n - 1] + P(n) - P(n - 1))
+donde P(i) - P(i - 1) calcula la diferencia entre el dia de compra y venta. En otras palabras, compro y vendo el mismo dia o compre antes y verifico cuanto mejoro por no haber vendido ayer. (NO ME ANDUVO)
+
+Otra manera seria pensar ¿vendo o no vendo hoy?:
+	- La mejor solución hasta el día i - 1 (no vendo);
+	- La mejor ganancia vendiendo exactamente en el día i (vendo).
+Por lo que la ecuacion de recurrencia seria:
+	OPT[i] = max(OPT[i - 1], p[i] - (min_precio_hasta_i - 1))
 '''
 
+def crear_opt(p):
+	OPT = [0] * len(p)
+
+	min_precio = p[0]
+	min_dia = 0
+	dia_compra = 0
+	dia_venta = 0
+
+	for i in range(1, len(p)):
+		if p[i] - min_precio > OPT[i - 1]:
+			OPT[i] = p[i] - min_precio
+			if OPT[i] > OPT[dia_venta]:
+				dia_compra = min_dia
+				dia_venta = i
+		else:
+			OPT[i] = OPT[i - 1]
+
+		if p[i] < min_precio:
+			min_precio = p[i]
+			min_dia = i
+
+	return OPT, (dia_compra, dia_venta)
+
 def compra_venta(p):
-	return 0, len(p) - 1
+	if len(p) < 2:
+		return (0, 0)
+	OPT, res = crear_opt(p)
+	return res
+
+'''
+Complejidad:
+	- O(n), siendo n la cantidad de predicciones del arreglo.
+'''

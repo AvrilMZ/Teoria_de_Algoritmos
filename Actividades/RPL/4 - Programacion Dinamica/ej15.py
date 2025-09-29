7c8ca1a6c14694f364aaad0cb7cc8cdd5e0561fd
AvrilMZ <amorfeo@fi.uba.ar>
2025-09-28 22:58:36 -0300

    Resuelto

--- Actividades/RPL/4 - Programacion Dinamica/ej15.py
+++ Actividades/RPL/4 - Programacion Dinamica/ej15.py
@@ -14,17 +14,39 @@ Nota sobre RPL: en este ejercicio se pide cumplir la tarea "utilizando programac
 '''
 
 '''
-Para este problema tenemos dos opciones por cada iteracion:
-	i. Cortar la soga
-	ii. No cortar la soga
-Dadas estas opciones podemos plantear la ecuacion de recurrencia como:
-	OPT[n] = max(max(i, OPT[i]) * max(i - 1, OPT[i - 1])), i -> [1, n - 1]
+Para este problema podemos plantear cortar la soga en una posicion i tal que 1 <= i <= n - 1, entonces ahora debemos tomar la decision de:
+	i. Cortar más algún segmento de la soga (usar el OPT del segmento);
+	ii. No cortar más ninguna parte, usando su optimo actual (usar la longitud tal cual).
+Osea, como buscamos el producto maximo:
+	max(i, OPT[i]) * max(i - 1, OPT[i - 1]) -> (producto maximo del primer segmento) * (producto maximo del segundo segmento)
+Luego debemos forzar todos los cortes de i para luego decidir cual es el que maximiza:
+	OPT[n] = max_{1 ≤ i ≤ n - 1}(max(i, OPT[i]) * max(i - 1, OPT[i - 1]))
 '''
 
+def crear_opt(n):
+	OPT = [0] * (n + 1)
+	OPT[1] = 1
+	OPT[2] = 1
+	for i in range(2, n + 1):
+		max_prod = 0
+		for j in range(1, i): # Cortes del primer segmento de la soga
+			prod = max(j, OPT[j]) * max(i - j, OPT[i - j])
+			if prod > max_prod:
+				max_prod = prod
+		OPT[i] = max_prod
+	return OPT
+
 def problema_soga(n):
-	if n <= 1:
+	if n == 0:
 		return 0
-	elif n == 2:
+	elif n == 1 or n == 2:
 		return 1
-	
-	return 0
+	OPT = crear_opt(n)
+	return OPT[n]
+
+'''
+Complejidad:
+	- O(n * m), siendo n la cantidad de metros de la soga y m la cantidad de subcortes que se realizaron. Aunque cuando i = n, m = n.
+Por lo que la complejidad final es:
+	O(n^2)
+'''

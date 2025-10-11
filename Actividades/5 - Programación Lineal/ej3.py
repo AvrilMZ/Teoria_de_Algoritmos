399b8779daf9cca818296119ecd1f3a40d88b5a3
AvrilMZ <amorfeo@fi.uba.ar>
2025-10-10 23:49:38 -0300

    Resuelto

---
+++ Actividades/5 - Programación Lineal/ej3.py
@@ -0,0 +1,12 @@
+'''
+Implementar un modelo de programación lineal que resuelva el problema de Vertex Cover mínimo (ejercicio 13 de BT).
+'''
+
+'''
+X: "Constante, vertice"
+V: "Variable binaria, vertice visitado o no"
+
+\min{\sum_{n = 0}^{n} X_i * V_i}
+
+donde V_i + V_j > 0 -> para todo (i, j) que pertenece al grafo
+'''

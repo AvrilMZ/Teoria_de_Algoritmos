792c25477c0c563970fc1c993d41ad8631be6bb4
AvrilMZ <amorfeo@fi.uba.ar>
2025-10-10 23:37:02 -0300

    Resuelto

---
+++ Actividades/5 - Programación Lineal/ej2.py
@@ -0,0 +1,12 @@
+'''
+Implementar un modelo de programación lineal que resuelva el problema de Juan El Vago (ejercicio 4 de PD).
+'''
+
+'''
+G: "Constante, ganancia del dia"
+T: "Variable binaria, trabajo o no trabajo"
+
+\max{\sum_{i = 1}^{n} G_i * T_i}
+
+donde T_i + T_{i + 1} <= 1
+'''

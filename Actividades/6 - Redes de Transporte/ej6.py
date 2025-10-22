fec5313576ad84acb8ccd25a88688742ce48c46d
AvrilMZ <amorfeo@fi.uba.ar>
2025-10-21 23:29:12 -0300

    Resuelto

--- Actividades/6 - Redes de Transporte/ej6.py
+++ Actividades/6 - Redes de Transporte/ej6.py
@@ -4,22 +4,22 @@ Luego, definir cuáles son los dos conjuntos del corte mínimo en dicha red.
 '''
 
 def obtener_flujo():
-    flujo = {}
-    vertices = "super_fuente", "S", "T", "U", "V", "W", "X", "Z"
-    flujo[("super_fuente", "S")] = 6 # Segun las pruebas aca iria 7, pero no entiendo ya que la misma cantidad de entrada deberia ser que la de salida.
-    flujo[("super_fuente", "X")] = 3
-    flujo[("S", "V")] = 3
-    flujo[("S", "U")] = 3
-    flujo[("V", "T")] = 3
-    flujo[("V", "W")] = 0
-    flujo[("W", "T")] = 6
-    flujo[("U", "W")] = 6
-    flujo[("X", "Z")] = 3
-    flujo[("Z", "W")] = 0
-    flujo[("Z", "U")] = 3
-    flujo[("U", "Z")] = 0
+	flujo = {}
+	vertices = "super_fuente", "S", "T", "U", "V", "W", "X", "Z"
+	flujo[("super_fuente", "S")] = 7
+	flujo[("super_fuente", "X")] = 2
+	flujo[("S", "V")] = 4
+	flujo[("S", "U")] = 3
+	flujo[("V", "T")] = 3
+	flujo[("V", "W")] = 1
+	flujo[("W", "T")] = 6
+	flujo[("U", "W")] = 4
+	flujo[("X", "Z")] = 2
+	flujo[("Z", "W")] = 1
+	flujo[("Z", "U")] = 1
+	flujo[("U", "Z")] = 0
 
-    conjunto_super_fuente = ["S", "X", "super_fuente"]
-    conjunto_sumidero = ["V", "W", "T"]
+	conjunto_super_fuente = ["super_fuente", "S", "U", "V", "W", "X", "Z"]
+	conjunto_sumidero = ["T"] # Complemento de conjunto super fuente
 
-    return flujo, conjunto_super_fuente, conjunto_sumidero
+	return flujo, conjunto_super_fuente, conjunto_sumidero

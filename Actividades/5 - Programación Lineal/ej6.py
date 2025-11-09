ec56f9dd44fe18fce0f6830f0bdef5b63a5fe9bf
Avril <amorfeo@fi.uba.ar>
2025-11-08 23:02:16 -0300

    Resuelto

---
+++ Actividades/5 - Programación Lineal/ej6.py
@@ -0,0 +1,13 @@
+'''
+Implementar un modelo de programación lineal que resuelva el problema de Independent Set Máximo.
+'''
+
+'''
+W_i: "Constante continua, peso del vertice i"
+X_i: "Variable binaria, vertice i agregado"
+
+\max(\sum_{i = 0}^{n} W_i * X_i) -> Contabilizo el peso solo de los vertices incluidos en la solucion
+
+Donde:
+    - X_i + \sum_{a pertenece a los adyacentes de i} X_a <= 1
+'''
